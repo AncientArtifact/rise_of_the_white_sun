befb0d4e5dd2ea8c64f3fe91d57ba2f1d6dea1ff
AncientArtifact <ancientartifact99@gmail.com>
2026-09-19 19:31:07 +0800

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -195,6 +195,19 @@ window.hideMap = function() {
     });
 }
 
+  function updateTabs() {
+    document.getElementById("powerstruggle_tab").style.display =
+        Q.sunyatsen_dead === 1 ? "block" : "none";
+
+    document.getElementById("northernexpedition_tab").style.display =
+        Q.northernexpedition_begun === 1 ? "block" : "none";
+
+    document.getElementById("wuhannanjing_tab").style.display =
+        Q.wuhannanjing_split === 1 ? "block" : "none";
+
+    document.getElementById("civilwar_tab").style.display =
+        Q.cpc_civilwar === 1 ? "block" : "none";
+}
   
   // This function allows you to modify the text before it's displayed.
   // E.g. wrapping chat-like messages in spans.
