d400e1a03a4b0907322cb996f9c5b97841af2041
AncientArtifact <ancientartifact99@gmail.com>
2026-09-19 20:08:12 +0800

    Change tab display logic to use 'inline' style

--- out/html/game.js
+++ out/html/game.js
@@ -199,16 +199,16 @@ window.hideMap = function() {
     const qualities = window.dendryUI.dendryEngine.state.qualities;
 
     document.getElementById("powerstruggle_tab").style.display =
-        qualities.sunyatsen_dead === 1 ? "" : "none";
+        qualities.sunyatsen_dead === 1 ? "inline" : "none";
 
     document.getElementById("northernexpedition_tab").style.display =
-        qualities.northern_expedition_begun === 1 ? "" : "none";
+        qualities.northern_expedition_begun === 1 ? "inline" : "none";
 
     document.getElementById("wuhannanjing_tab").style.display =
-        qualities.wuhan_nanjing_split === 1 ? "" : "none";
+        qualities.wuhan_nanjing_split === 1 ? "inline" : "none";
 
     document.getElementById("civilwar_tab").style.display =
-        qualities.cpc_civilwar === 1 ? "" : "none";
+        qualities.cpc_civilwar === 1 ? "inline" : "none";
 };
   
   // This function allows you to modify the text before it's displayed.
